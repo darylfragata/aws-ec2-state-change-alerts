ff3c77a3206a4a955970d69517fb7ac15f321077
Daryl <151862534+darylfragata@users.noreply.github.com>
2025-05-14 23:16:17 +0800

    Update lambda_function.py
    Added other trigger action like RunInstances, RebootInstances, StopInstances, StartInstances, TerminateInstances

--- lambda_function.py
+++ lambda_function.py
@@ -10,56 +10,119 @@ ec2 = boto3.client('ec2')
 # Environment variable for SNS Topic ARN
 SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
 
-def get_instance_name(instance_id, region):
+
+def get_instance_details(instance_id, region):
     try:
-        ec2 = boto3.client('ec2', region_name=region)
-        response = ec2.describe_instances(InstanceIds=[instance_id])
-        tags = response['Reservations'][0]['Instances'][0].get('Tags', [])
-        for tag in tags:
-            if tag['Key'] == 'Name':
-                return tag['Value']
-        return "No Name Tag"
+        ec2_client = boto3.client('ec2', region_name=region)
+        response = ec2_client.describe_instances(InstanceIds=[instance_id])
+        instance = response['Reservations'][0]['Instances'][0]
+        tags = instance.get('Tags', [])
+        name_tag = next((tag['Value'] for tag in tags if tag['Key'] == 'Name'), 'No Name Tag')
+        instance_type = instance.get('InstanceType', 'Unknown')
+        platform = instance.get('Platform', 'Linux/UNIX')  # Default is Linux if key doesn't exist
+        return {
+            "instance_id": instance_id,
+            "name": name_tag,
+            "type": instance_type,
+            "platform": platform
+        }
     except Exception as e:
-        print(f"Error fetching Name tag for {instance_id}: {str(e)}")
-        return "Error Fetching Name Tag"
+        print(f"Error fetching instance details for {instance_id}: {str(e)}")
+        return {
+            "instance_id": instance_id,
+            "name": "Error",
+            "type": "Unknown",
+            "platform": "Unknown"
+        }
+
+
+def handle_run_instances(event, region, account_id):
+    items = event["detail"]["responseElements"]["instancesSet"]["items"]
+    instance_ids = [item["instanceId"] for item in items]
+    return extract_message(event, region, account_id, instance_ids, "EC2 Instance(s) Launched")
+
+
+def handle_reboot_instances(event, region, account_id):
+    items = event["detail"]["requestParameters"]["instancesSet"]["items"]
+    instance_ids = [item["instanceId"] for item in items]
+    return extract_message(event, region, account_id, instance_ids, "EC2 Instance(s) Rebooted")
+
+
+def handle_stop_instances(event, region, account_id):
+    items = event["detail"]["requestParameters"]["instancesSet"]["items"]
+    instance_ids = [item["instanceId"] for item in items]
+    return extract_message(event, region, account_id, instance_ids, "EC2 Instance(s) Stopped")
+
+
+def handle_start_instances(event, region, account_id):
+    items = event["detail"]["requestParameters"]["instancesSet"]["items"]
+    instance_ids = [item["instanceId"] for item in items]
+    return extract_message(event, region, account_id, instance_ids, "EC2 Instance(s) Started")
+
+
+def handle_terminate_instances(event, region, account_id):
+    items = event["detail"]["requestParameters"]["instancesSet"]["items"]
+    instance_ids = [item["instanceId"] for item in items]
+    return extract_message(event, region, account_id, instance_ids, "EC2 Instance(s) Terminated")
+
+
+def extract_message(event, region, account_id, instance_ids, subject):
+    instance_details = []
+    for instance_id in instance_ids:
+        details = get_instance_details(instance_id, region)
+        instance_details.append(
+            f"Instance: {details['instance_id']}\n"
+            f"Name: {details['name']}\n"
+            f"Type: {details['type']}\n"
+            f"Platform: {details['platform']}"
+        )
+
+    user = event["detail"]["userIdentity"].get("userName", "Unknown")
+    time = event["time"]
+    message = (
+        f"{subject}\n\n"
+        f"{chr(10).join(instance_details)}\n\n"
+        f"Initiated By: {user}\n"
+        f"AWS Account: {account_id}\n"
+        f"Region: {region}\n"
+        f"Time: {time} UTC\n\n\n\n\n\n\n\n\n\n"
+    )
+    return subject, message
 
 
 def lambda_handler(event, context):
     try:
         print("Received event:", json.dumps(event, indent=2))
-
-        instance_details = []
         region = event["region"]
-        account_id = event["account"]  # Added AWS account ID
-        items = event["detail"]["responseElements"]["instancesSet"]["items"]
+        account_id = event["account"]
+        event_name = event["detail"]["eventName"]
 
-        for item in items:
-            instance_id = item["instanceId"]
-            name_tag = get_instance_name(instance_id, region)
-            instance_details.append(f"Instance: {instance_id}\nName: {name_tag}")
-
-        user = event["detail"]["userIdentity"].get("userName", "Unknown")
-        time = event["time"]
-
-        # Including AWS Account ID in the message
-        message = (
-            f"New EC2 Instance(s) Launched\n\n"
-            f"{chr(10).join(instance_details)}\n\n"
-            f"Launched By: {user}\n"
-            f"AWS Account: {account_id}\n"  # Added AWS Account
-            f"Region: {region}\n"
-            f"Time: {time} UTC"
-        )
+        if event_name == "RunInstances":
+            subject, message = handle_run_instances(event, region, account_id)
+        elif event_name == "RebootInstances":
+            subject, message = handle_reboot_instances(event, region, account_id)
+        elif event_name == "StopInstances":
+            subject, message = handle_stop_instances(event, region, account_id)
+        elif event_name == "StartInstances":
+            subject, message = handle_start_instances(event, region, account_id)
+        elif event_name == "TerminateInstances":
+            subject, message = handle_terminate_instances(event, region, account_id)
+        else:
+            print(f"Unhandled event type: {event_name}")
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"message": "Unhandled event", "eventName": event_name})
+            }
 
         sns.publish(
             TopicArn=SNS_TOPIC_ARN,
-            Subject="Alert: EC2 Instance Launched",
+            Subject=f"Alert: {subject}",
             Message=message
         )
 
         return {
             "statusCode": 200,
-            "body": json.dumps({"message": "Notification sent", "instances": instance_details})
+            "body": json.dumps({"message": "Notification sent", "eventName": event_name})
         }
 
     except Exception as e:
